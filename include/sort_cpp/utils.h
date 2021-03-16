3c10a2d28e0e28ab7337e760a690ab81c712d375
Adi Vardi <adi.vardi@enway.ai>
2021-03-16 18:47:12 +0800

    tune utils

--- include/sort_cpp/utils.h
+++ include/sort_cpp/utils.h
@@ -1,10 +1,10 @@
 #pragma once
 
-constexpr int kNumColors = 32;
+// constexpr int kNumColors = 32;
 
-constexpr int kMaxCoastCycles = 1;
+constexpr int kMaxCoastCycles = 3;  // maximum time to continue tracking without fitting observation
 
-constexpr int kMinHits = 3;
+constexpr int kMinHits = 1;   // minimum times updated by observation that fit the prediction (only for vis)
 
-// Set threshold to 0 to accept all detections
-constexpr float kMinConfidence = 0.6;
+// // Set threshold to 0 to accept all detections
+// constexpr float kMinConfidence = 0.6;
