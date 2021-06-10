e7d917dfc67c89eb955b1f2bbed1e95a0778422e
Adi Vardi <adi.vardi@enway.ai>
2021-06-10 15:36:16 +0800

    more info in check_blocked

--- scripts/check_blocked.py
+++ scripts/check_blocked.py
@@ -7,11 +7,15 @@ import os
 import rosbag
 import rospy
 
-folder = "/home/adi/Projects/traffic/ground_removal/"
-topic = '/navigation/dynamic_path_checker/blocked'
+folder = "/home/adi/Projects/traffic/ground_removal/cetran-beae5851e_heightmap+ring+minhits1_2021-06-08-17-53-31"
 
-SHOW = True
+blocked_topic = '/navigation/dynamic_path_checker/blocked'
+vel_topic = '/donner_articulated_drive_controller/odom'
+cmdvel_topic = '/cmd_vel'
+
+SHOW = False
 SAVE = True
+PLOT_SPEED = False
 
 files = glob(os.path.join(folder, '*.bag'))
 
@@ -25,7 +29,10 @@ for file in files:
 
     time_list = list()
     blocked_data = list()
-    # blocked_bool = list()
+    vel_time_list = list()
+    vel_list = list()
+    cmdvel_time_list = list()
+    cmdvel_list = list()
 
     blocked_msgs_count = 0
     total_msgs_count = 0
@@ -33,25 +40,39 @@ for file in files:
     last_free_timestamp = None
     free_time = rospy.Duration(0, 0)
 
-    for _, message, time in bag.read_messages(topic):
-        time_list.append(time.to_sec())
-        blocked_data.append(message.data)
-        total_msgs_count += 1
+    for topic, message, time in bag.read_messages([blocked_topic, vel_topic, cmdvel_topic]):
+        if topic == blocked_topic:
+            time_list.append(time.to_sec())
+            blocked_data.append(message.data)
+            total_msgs_count += 1
 
-        if message.data > -1.0:
-            blocked_msgs_count += 1
-            if last_msg_free:
-                free_time += (time - last_free_timestamp)
-            last_msg_free = False
-            # blocked_bool.append(1)
-        else:
-            last_msg_free = True
-            last_free_timestamp = time
-            # blocked_bool.append(0)
+            if message.data > -1.0:
+                blocked_msgs_count += 1
+                if last_msg_free:
+                    free_time += (time - last_free_timestamp)
+                last_msg_free = False
+                # blocked_bool.append(1)
+            else:
+                last_msg_free = True
+                last_free_timestamp = time
+                # blocked_bool.append(0)
+
+        elif topic == vel_topic:
+            vel_time_list.append(time.to_sec())
+            vel_list.append(message.twist.twist.linear.x)
+
+        elif topic == cmdvel_topic:
+            cmdvel_time_list.append(time.to_sec())
+            cmdvel_list.append(message.drive.speed)
 
     bag.close()
 
     print(f"---- {filename} ----")
+
+    if total_msgs_count == 0 or len(blocked_data) == 0:
+        print("no blocked data")
+        continue
+
     # print(f"start time : {start_time}")
     # print(f"end time : {end_time}")
     # print(f"total_time : {total_time}")
@@ -65,6 +86,10 @@ for file in files:
 
     blocked_data = np.array(blocked_data).astype(np.float)
     time_list = np.array(time_list).astype(np.float)
+    vel_list = np.array(vel_list).astype(np.float)
+    vel_time_list = np.array(vel_time_list).astype(np.float)
+    cmdvel_list = np.array(cmdvel_list).astype(np.float)
+    cmdvel_time_list = np.array(cmdvel_time_list).astype(np.float)
 
     f = plt.figure(figsize=(14, 9))
     plt.suptitle(filename)
@@ -72,10 +97,16 @@ for file in files:
     plt.title(f"free time: {free_time_percentage:.3f}%")
     plt.plot(time_list - time_list[0], blocked_data, linestyle='--',
              linewidth=1, marker='.', color='b', label='blocked')
+    if PLOT_SPEED:
+        plt.plot(vel_time_list - vel_time_list[0], vel_list, linestyle='--',
+                 linewidth=1, markersize=2, marker='.', color='r', label='vel')
+        plt.plot(cmdvel_time_list - cmdvel_time_list[0], cmdvel_list, linestyle='--',
+                 linewidth=1, markersize=2, marker='.', color='g', label='cmdvel')
     # plt.plot(time_list - time_list[0], blocked_bool, linestyle='-',
     #          linewidth=1, marker='.', color='r', label='bool')
     # plt.ylim([-2, 45])
-    plt.xlim([-5, 140])
+    plt.xlim([-5, 175])
+    # plt.xlim([-5, 590])
     plt.legend()
     plt.grid()
 
